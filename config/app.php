b93500a32b21a874f731687439c2c33b649f8d47
Mobina Jafarian <mobina.nezhadjfarian@gmail.com>
2023-07-09 23:55:46 +0330

    define current_route

--- config/app.php
+++ config/app.php
@@ -1,5 +1,9 @@
 <?php
 
-define("APP_NAME",'MY_FRAMEWORK');
+define("APP_NAME", 'MY_FRAMEWORK');
 define("BASE_URL", "https://$_SERVER[HTTP_HOST]:8000");
 define("BASE_DIR", realpath(__DIR__ . "/../"));
+
+$current_route = explode('?', $_SERVER['REQUEST_URI'])[0];
+$current_route = substr($current_route, 1);
+define('CURRENT_ROUTE', $current_route);
