3d72cf4744325f88fd5ca897b590a828dd1f2a14
Mobina Jafarian <mobina.nezhadjfarian@gmail.com>
2023-07-09 23:56:24 +0330

    add composer to project

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '36639c181be369feb8a8f9d4b4ac9243c389bb8e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '36639c181be369feb8a8f9d4b4ac9243c389bb8e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
