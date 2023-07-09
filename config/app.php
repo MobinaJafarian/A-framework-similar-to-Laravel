0eb19674ebbc1bdbeb179565489581c3247b1777
Mobina Jafarian <mobina.nezhadjfarian@gmail.com>
2023-07-09 23:33:18 +0330

    update app_name

--- config/app.php
+++ config/app.php
@@ -1,5 +1,5 @@
 <?php
 
-define("APP_NAME",'MY FRAMEWORK');
+define("APP_NAME",'MY_FRAMEWORK');
 define("BASE_URL", "https://$_SERVER[HTTP_HOST]:8000");
 define("BASE_DIR", realpath(__DIR__ . "/../"));
