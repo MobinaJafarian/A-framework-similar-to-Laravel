67c24be4d0da3a15ad00f219fb71c111659eaf3e
Mobina Jafarian <mobina.nezhadjfarian@gmail.com>
2023-07-14 13:50:01 +0330

    update core Route

--- core/Router/Web/Route.php
+++ core/Router/Web/Route.php
@@ -8,36 +8,44 @@ class Route
     public static function get($route, $context)
     {
         $controller = $context[0];
+        $controller = explode("\\", $controller);
+        $controller = array_splice($controller, -1)[0];
         $method = $context[1];
 
         global $routes;
-        array_push($routes['get'], array('route' => $route, 'controller' => $controller, 'method' => $method));
+        array_push($routes['get'], array('route' => trim($route, "/ "), 'controller' => $controller, 'method' => $method));
     }
 
     public static function post($route, $context)
     {
         $controller = $context[0];
+        $controller = explode("\\", $controller);
+        $controller = array_splice($controller, -1)[0];
         $method = $context[1];
 
         global $routes;
-        array_push($routes['post'], array('route' => $route, 'controller' => $controller, 'method' => $method));
+        array_push($routes['post'], array('route' => trim($route, "/ "), 'controller' => $controller, 'method' => $method));
     }
 
     public static function put($route, $context)
     {
         $controller = $context[0];
+        $controller = explode("\\", $controller);
+        $controller = array_splice($controller, -1)[0];
         $method = $context[1];
 
         global $routes;
-        array_push($routes['put'], array('route' => $route, 'controller' => $controller, 'method' => $method));
+        array_push($routes['put'], array('route' => trim($route, "/ "), 'controller' => $controller, 'method' => $method));
     }
 
     public static function delete($route, $context)
     {
         $controller = $context[0];
+        $controller = explode("\\", $controller);
+        $controller = array_splice($controller, -1)[0];
         $method = $context[1];
 
         global $routes;
-        array_push($routes['delete'], array('route' => $route, 'controller' => $controller, 'method' => $method));
+        array_push($routes['delete'], array('route' => trim($route, "/ "), 'controller' => $controller, 'method' => $method));
     }
 }
