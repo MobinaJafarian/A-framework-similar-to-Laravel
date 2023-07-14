5c604926a1d9485cd548a02f9cee4c28ec76d333
Hind Sagar Biswas <hindsbhk@gmail.com>
2023-07-14 17:06:14 +0600

    loads env vars from .env

--- config/app.php
+++ config/app.php
@@ -1,7 +1,9 @@
 <?php
 
-define("APP_NAME", 'MY_FRAMEWORK');
-define("BASE_URL", "https://$_SERVER[HTTP_HOST]:8000");
+
+
+define("APP_NAME", $_ENV['APP_NAME']);
+define("BASE_URL", $_ENV['APP_URL']);
 define("BASE_DIR", realpath(__DIR__ . "/../"));
 
 $current_route = explode('?', $_SERVER['REQUEST_URI'])[0];
