090c021ac058e4446e1913b6ebf22de1b9f10bac
Mobina Jafarian <mobina.nezhadjfarian@gmail.com>
2023-07-15 23:38:08 +0330

    add checkRoute method

--- core/Router/Router.php
+++ core/Router/Router.php
@@ -2,12 +2,16 @@
 
 namespace Core\Router;
 
+use ReflectionMethod;
+
 class Router
 {
     private $current_route;
     private $method_field;
     private $routes;
-    private $params;
+    private $params = [];
+
+    private $result = false;
 
     public function __construct()
     {
@@ -31,4 +35,53 @@ class Router
         }
         return $method_field;
     }
+
+    public function checkRoute()
+    {
+
+        $reservedRoutes = $this->routes[$this->method_field];
+        foreach ($reservedRoutes as $reservedRoute) {
+
+            $reservedRouteArray = explode('/', $reservedRoute['route']);
+            if (sizeof($this->current_route) == sizeof($reservedRouteArray)) {
+
+                foreach ($reservedRouteArray as $key => $value) {
+
+                    if ($this->current_route[$key] == $value) {
+                        if (!empty($reservedRouteArray[$key + 1])) {
+                            if (substr($reservedRouteArray[$key + 1], 0, 1) == "{" &&
+                                substr($reservedRouteArray[$key + 1], -1) == "}") {
+                                array_push($this->params, $this->current_route[$key + 1]);
+                            } else {
+                                $this->params = [];
+                            }
+                        }
+
+                        $controller = "\App\Http\Controllers\\" . $reservedRoute['controller'];
+
+                        $object = new $controller();
+                        if (method_exists($object, $reservedRoute['method'])) {
+                            $reflection = new ReflectionMethod($controller, $reservedRoute['method']);
+                            $parameterCount = $reflection->getNumberOfParameters();
+                            if ($parameterCount <= count($this->params)) {
+                                call_user_func_array([$object, $reservedRoute['method']], $this->params);
+                            }
+
+                        }
+
+                        $this->result = true;
+
+                    }
+
+                }
+            }
+
+        }
+
+        if (!$this->result) {
+            echo 'controller not found';
+        }
+
+    }
+
 }
