a9fe573017207a08daf5b5ee50f0f183d9ba3cb0
Mobina Jafarian <mobina.nezhadjfarian@gmail.com>
2023-08-03 22:12:03 +0330

    add HasCRUD trait

---
+++ core/Database/Traits/HasCRUD.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Core\Database\Traits;
+
+use Core\Database\DBConnection\DBConnection;
+
+trait HasCRUD
+{
+    protected function setFillabels($array)
+    {
+        $fillables = [];
+        foreach ($this->fillable as $attribute) {
+            array_push($fillables, $attribute . " = ?");
+            $this->setValue($attribute, $array[$attribute]);
+        }
+        return implode(', ', $fillables);
+    }
+
+    protected function insert($array)
+    {
+        $this->setSql("INSERT INTO {$this->table} SET " . $this->setFillabels($array) . "," . $this->createdAt . "=Now();");
+        $this->executeQuery();
+        $this->resetQuery();
+    }
+
+    public function update($array)
+    {
+        $this->setSql("UPDATE " . $this->table . " SET " . $this->setFillabels($array) . ", " . $this->updatedAt . "=Now()");
+        $this->setWhere("AND ", $this->primaryKey . " = ?");
+        $this->setValue($this->primaryKey, $this->{$this->primaryKey});
+        $this->executeQuery();
+        $this->resetQuery();
+        return $this;
+    }
+}
